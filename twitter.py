02522b4be1811a9e2be764a791ff4576d2106f15
sshanbh2 <sshanbh2@ncsu.edu>
2018-10-26 16:31:37 -0400

    Delete twitter.py

--- twitter.py
+++
@@ -1,5 +0,0 @@
-import twitter
-api=twitter.Api(consumer_key='IFPYrIpyKNqt8qe7GbQvw8NzQ',consumer_secret='mo2eR5eoGLu595DaSKszFanUWcvYpTHKRIaRyvGnYHA4jUXOKi',access_token_key='779885459929337856-y0LNiqvm4EsFWDguaLW712p8e9c5MdH',access_token_secret='tvxnYaWGgTaL1cwDF0QAGBKkeBvqYBSEgzbM9GwsUqkIX')
-t=api.GetFavorites(screen_name="aishwaryassr1",count=10);
-tweets = [i.AsDict() for i in t]
-print (tweets)
