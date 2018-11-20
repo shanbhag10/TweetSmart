5a3c3c1cb051fdf3972234aed8dbf26fe1e40889
sshanbh2 <sshanbh2@ncsu.edu>
2018-11-20 00:41:32 -0500

    Update twitter_likes.py

--- code/twitter_likes.py
+++ code/twitter_likes.py
@@ -3,29 +3,6 @@ import csv
 import pandas as pd
 import heapq 
 
-
-class FreqStack(object):
-
-    def __init__(self):
-        self.freq = collections.Counter()
-        self.group = collections.defaultdict(list)
-        self.maxfreq = 0
-
-    def push(self, x):
-        f = self.freq[x] + 1
-        self.freq[x] = f
-        if f > self.maxfreq:
-            self.maxfreq = f
-        self.group[f].append(x)
-
-    def pop(self):
-        x = self.group[self.maxfreq].pop()
-        self.freq[x] -= 1
-        if not self.group[self.maxfreq]:
-            self.maxfreq -= 1
-
-        return x
-
 # Twitter API credentials
 consumer_key = 'IFPYrIpyKNqt8qe7GbQvw8NzQ'
 consumer_secret = 'mo2eR5eoGLu595DaSKszFanUWcvYpTHKRIaRyvGnYHA4jUXOKi'
