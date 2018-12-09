363e9c1d7e5bf2f68ffc8d87f34378ad9dccf118
sshanbh2 <sshanbh2@ncsu.edu>
2018-12-08 19:22:31 -0500

    setup

--- env_setup.sh
+++ env_setup.sh
@@ -8,7 +8,8 @@ pip3 install ansible
 pip3 install selenium
 cd /tmp/
 wget -q -O - "https://dl-ssl.google.com/linux/linux_signing_key.pub" | sudo apt-key add -
-sudo sed -i '$ a deb http://dl.google.com/linux/chrome/deb/ stable main' /etc/apt/sources.list
+sudo vim /etc/apt/sources.list
+sudo apt-get update
 sudo apt-get -y install google-chrome-stable
 sudo apt-get -y install xvfb
 sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
@@ -18,3 +19,5 @@ unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 mkdir /home/ubuntu/TweetSmart/selenium_data
 mkdir /home/ubuntu/TweetSmart/selenium_total
+
+#
